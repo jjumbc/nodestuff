8aae59272ae8d63e2018f13fffb85d63a365402f
Justus <actionjackson0@gmail.com>
2018-02-04 13:18:43 -0500

    some mongo stuff

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var PORT = process.env.PORT;
 var express = require('express');
 var app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+const MONGO_URI = 'mongodb://127.0.0.1:27017/ng'
+
 app.listen(PORT, function() {
-	console.log('Server running, port ${PORT}`');
+	console.log(`Server running, port ${PORT}`);
 });
 
 app.get('/', function(request, response) {
